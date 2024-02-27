6873ae33f0d0e9b0418eedb284a5c8f2505793a4
Adon <a@atriskmedia.com>
2024-02-27 00:22:06 -0500

    added oauth keys

--- install_drupal.sh
+++ install_drupal.sh
@@ -5,10 +5,17 @@ DB_NAME=oak
 DB_USER=demo
 DB_PASSWORD=demo
 ACCOUNT_PASSWORD=demo
+REPO=~/src/tractstack-drupal/
 
 composer create-project drupal/recommended-project "$NAME"
 cd "$NAME"
+mkdir oauth_keys
+cd oauth_keys
+cp "$REPO"/conf/oauth/web.config .
 cp -r ~/src/tractstack-drupal/profiles/ web/profiles
+openssl genrsa -out private.key 2048
+openssl rsa -in private.key -pubout >public.key
+cd ..
 composer require --dev drush/drush
 composer require drupal/jsonapi_extras
 composer require drupal/simple_oauth
